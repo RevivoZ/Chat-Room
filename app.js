dbb752f58d7a3ec3d2a6b750c00df198565b9f98
Unknown <kfda89@gmail.com>
2017-10-14 16:44:10 +0300

    bug fixed in online-users

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-var io = require('socket.io')(serv, {});
-
-app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/client/index.html');
+var io = require('socket.io')(serv,{});
+var users = [];
+app.get('/',function (req, res){
+    res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 serv.listen(80);
@@ -12,21 +12,24 @@ serv.listen(80);
 console.log('server Started');
 
 
-io.sockets.on('connection', function (socket) {
-	console.log('new user');
-	socket.on('nickName', function (data) {
-		socket.name = data;
-		console.log(socket.name + ' is IN');
-	})
-	socket.on('newMsg', function (data) {
-		console.log('new Mgs from ' + data.user + '<br>' + 'and the msg is : ' + data.msg);
-		io.sockets.emit('globalMsg', data)
-	})
+io.sockets.on('connection', function(socket){
+  console.log('new user');
 
-	socket.on('userSaid', function (data) {
+  socket.on('nickName' , function (data){
+    socket.name = data;
+    users.push(data);
+    io.sockets.emit('online_users' , users);
+    console.log(socket.name + ' is IN');
+    console.log(users);
+  })
 
-		console.log(' someOneSaid ' + data.value);
-		io.sockets.emit('someOneSaid', data.value);
+  socket.on('newMsg' , function (data){
+    console.log('new Mgs from ' + data.user + '<br>' + 'and the msg is : ' + data.msg);
+    io.sockets.emit('globalMsg' , data)
+    } )
 
-	})
+  socket.on('userSaid' , function (data){
+    console.log(' someOneSaid ' + data.value);
+    io.sockets.emit('someOneSaid' , data.value);
+    })
 });
--- client/main.js
+++ client/main.js
@@ -2,21 +2,30 @@ var socket = io();
 var chat;
 var nickName;
 var publicChat;
+var onlineUsers = document.getElementById('onlineUsers');
 
 window.onload = function(){
   $('#nickName').focus();
+
 }
+
 function _send(event){
-  if (event== null || event.keyCode == 13){
+  // event = event || window.event;
+  if (event && event.keyCode != 13) return;
   nickName = document.getElementById('nickName').value;
-  document.body.innerHTML = '<input type="text" id="msg" onkeypress="newMsg(event)"><input type="button" id="send" value="send" onclick="newMsg()"><div id="publicChat"></div>';
+  document.getElementById('container').innerHTML = '<input type="text" id="msg" onkeypress="newMsg(event)"><input type="button" id="send" value="send" onclick="newMsg()"><div id="publicChat"></div>';
   publicChat = document.getElementById('publicChat');
  socket.emit('nickName' , nickName );
   socket.emit('newMsg' , {user : nickName , msg : ' Is In'})
   $('#msg').focus();
 }
+socket.on('online_users' , function (data){
+  console.log(data);
+  onlineUsers.innerHTML = '';
+for (var i = 0; i < data.length; i++) {
+  onlineUsers.innerHTML += '<p>'+ data[i] +'</p>';
 }
-
+});
 
 socket.on('someOneSaid' , function (data){
   console.log('fds');
