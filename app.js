eab16662dfe3e50c98ee526db2d259255a746d0b
RevivoZ <hadar2703@gmail.com>
2017-10-14 16:04:23 +0300

    Another small change

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-var io = require('socket.io')(serv,{});
+var io = require('socket.io')(serv, {});
 
-app.get('/',function (req, res){
-    res.sendFile(__dirname + '/client/index.html');
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client',express.static(__dirname + '/client'));
-serv.listen(2000);
+app.use('/client', express.static(__dirname + '/client'));
+serv.listen(80);
 
 console.log('server Started');
 
 
-io.sockets.on('connection', function(socket){
-  console.log('new user');
-  socket.on('nickName' , function (data){
-    socket.name = data;
-    console.log(socket.name + ' is IN');
-  })
-  socket.on('newMsg' , function (data){
-    console.log('new Mgs from ' + data.user + '<br>' + 'and the msg is : ' + data.msg  );
-    io.sockets.emit('globalMsg' , data)
-    } )
+io.sockets.on('connection', function (socket) {
+	console.log('new user');
+	socket.on('nickName', function (data) {
+		socket.name = data;
+		console.log(socket.name + ' is IN');
+	})
+	socket.on('newMsg', function (data) {
+		console.log('new Mgs from ' + data.user + '<br>' + 'and the msg is : ' + data.msg);
+		io.sockets.emit('globalMsg', data)
+	})
 
-  socket.on('userSaid' , function (data){
+	socket.on('userSaid', function (data) {
 
-    console.log(' someOneSaid ' + data.value);
-    io.sockets.emit('someOneSaid' , data.value);
+		console.log(' someOneSaid ' + data.value);
+		io.sockets.emit('someOneSaid', data.value);
 
-    })
+	})
 });
